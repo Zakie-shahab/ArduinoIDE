4b7fe9d42ff8529e2176bb936cb1d148447ef631
Muhammad Zakie Shahab <81218897+Zakie-shahab@users.noreply.github.com>
2025-05-15 20:23:29 +0700

    Update rf_receive.ino

--- Base/rf_receive/rf_receive.ino
+++ Base/rf_receive/rf_receive.ino
@@ -6,7 +6,7 @@ RH_ASK rf_driver;
 void setup() {
   Serial.begin(115200);
   if (!rf_driver.init()) {
-    Serial.println("RF module initialization failed!");
+    Serial.println("RF module gagal!");
   }
 }
 
@@ -14,7 +14,6 @@ void loop() {
   uint8_t buf[RH_ASK_MAX_MESSAGE_LEN];
   uint8_t buflen = sizeof(buf);
 
-  // Check if a message was received
   if (rf_driver.recv(buf, &buflen)) {
     Serial.print("Message received: ");
     Serial.println((char*)buf);
